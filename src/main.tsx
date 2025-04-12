f863743455d1e7ab6d593b80757991c8bad497a0
Jae-Hong-Kim <newfly101@naver.com>
2025-04-13 04:07:53 +0900

    chore: useEffect 호출 1회만 하기 위해서 strict모드 삭제
    ref: #4

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.scss'
 import AppRouter from "@/router";
+import { store } from '@/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AppRouter />
-  </StrictMode>,
+  // <StrictMode>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  // </StrictMode>,
 )
