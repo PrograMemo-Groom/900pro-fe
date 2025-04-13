6066c588d169a0e435d6f24c983f8fba614348fa
Jae-Hong-Kim <newfly101@naver.com>
2025-04-13 13:06:20 +0900

    chore: BE서버로 통신 완료 / data form 안맞음 - cors 오류의 경우 be 연결 설정 완료해야 함 ref: #4

--- src/store/api/ApiConfig.ts
+++ src/store/api/ApiConfig.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const BaseUrl: string = 'http://localhost:8080/api';
+const BaseUrl: string = 'http://ec2-3-39-135-118.ap-northeast-2.compute.amazonaws.com:8080/api';
 // const BaseUrl: string = '/api'; // 배포 시 스위치 가능
 
 const API: AxiosInstance = axios.create({
