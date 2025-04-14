e71bccceae585ce35dbb2cbf295e43b4f0f104cd
Kim Keon-Young <diamondgonny@gmail.com>
2025-04-14 17:16:19 +0900

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
       //   element: <CodingTestPage />,
       // },
       {
-        path: '/history',
+        path: 'history',
         element: <Chat />
       }
     ],
