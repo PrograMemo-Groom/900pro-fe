4700df9ee6928497b68e1f354b8300f936127e74
S2ej1n <ks320205@naver.com>
2025-04-15 14:11:53 +0900

    [design] 채팅 date 스타일링

--- src/pages/history/Chat.tsx
+++ src/pages/history/Chat.tsx
@@ -6,7 +6,7 @@ function Chat() {
     <div>
       <main className={styles.container}>
         <div className={styles.line}></div>
-        <p>2025년 4월 10일 목요일</p>
+        <p className={styles.date}>2025년 4월 10일 목요일</p>
       </main>
     </div>
   )
