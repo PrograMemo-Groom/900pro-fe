1fcbf1140f419b5f82929e49b7696924e887a10f
S2ej1n <ks320205@naver.com>
2025-04-15 16:41:45 +0900

    [design] 왼쪽 말풍선 시간 추가

--- src/pages/history/Chat.tsx
+++ src/pages/history/Chat.tsx
@@ -23,30 +23,33 @@ function Chat() {
         <p className={styles.date}>2025년 4월 10일 목요일</p>
 
         {/* 채팅봇 */}
+        <p className={styles.bubble_user_name}>🩵 알림봇</p>
         <div className={styles.bubble_container_left}>
-          <p className={styles.bubble_user_name}>🩵 알림봇</p>
           <div className={styles.bubble_chatbot}>
             <p>[&nbsp;&nbsp;&nbsp;&nbsp;25.04.10&nbsp;&nbsp;&nbsp;&nbsp;]</p>
             <p>응시하느라 고생하셨습니다 </p>
             <p>오늘의 문제번호 : <span>#121 #3999 #986</span></p>
             <button>문제 보러 가기</button>
           </div>
+          <p className={styles.time}>오후 3:44</p>
         </div>
 
         {/* 왼쪽 사용자 */}
+        <p className={styles.bubble_user_name}>유리미에오</p>
         <div className={styles.bubble_container_left}>
-          <p className={styles.bubble_user_name}>유리미에오</p>
           <div className={styles.bubble_left}>
             오늘 문제 저번에 공부한거 나온거 맞죠 ??? 👏🏻
           </div>
+          <p className={styles.time}>오후 3:44</p>
         </div>
 
+        <p className={styles.bubble_user_name}>거녕거녕</p>
         <div className={styles.bubble_container_left}>
-          <p className={styles.bubble_user_name}>거녕거녕</p>
           <div className={styles.bubble_left}>
-          어제 풀었던 #1253 번 맞나요 !!
-          비슷한 느낌이 나긴 했어요 ~ 근데 저는 이렇게 풀었는데 제 코드 보러 오실래요?
+            어제 풀었던 #1253 번 맞나요 !!
+            비슷한 느낌이 나긴 했어요 ~ 근데 저는 이렇게 풀었는데 제 코드 보러 오실래요?
           </div>
+          <p className={styles.time}>오후 3:44</p>
         </div>
       </main>
     </div>
