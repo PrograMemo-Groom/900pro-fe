d3d2cbab62aed119876226b973370588c6505bdf
Jae-Hong-Kim <newfly101@naver.com>
2025-04-21 16:14:46 +0900

    feature: [api] 비밀번호 초기화 페이지 api 연동 추가 > 이메일 인증 시 임시 비밀번호 입력 > 이메일로 날아온 임시 비밀번호 추가 ref: #18

--- src/pages/resetPassword/ResetPassword.tsx
+++ src/pages/resetPassword/ResetPassword.tsx
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
-import styles from "@/css/login/Auth.module.scss";
-import { LoginFormValues, LoginProps } from '@/pages/login/Login.interface.ts';
+import React, { useState } from 'react';
+import styles from '@/css/login/Auth.module.scss';
 import Landing from '@/pages/common/Landing.tsx';
-// import axios from "axios";
-// import type { AxiosResponse } from 'axios';
+import { ResetForm, verifyForm } from '@/pages/resetPassword/ResetPassword.interface.ts';
+import { AxiosResponse } from 'axios';
+import API from '@/store/api/ApiConfig.ts';
 
-const ResetPassword: React.FC<LoginProps> = ({initialValues}) => {
-  const [form, setForm] = useState<LoginFormValues>({
-    email: initialValues?.email || "",
-    password: initialValues?.password || ""
-  })
-  const handleOnSubmit = (e: React.FormEvent) => {
+const ResetPassword = () => {
+  const [form, setForm] = useState<ResetForm>({ email: '', password: '' });
+  const handleOnSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("form 실행", form);
-  }
+    console.log('form 실행', form.email);
+    try {
+      const response: AxiosResponse<ResetForm> = await API.post("/auth/verify", form);
+      console.log("이메일 인증 코드 요청 : ",response.data);
+    } catch (e) {
+      console.log("error : ", e.response.data?.message);
+    }
+  };
   const handleOnChange = (e: React.FormEvent) => {
-    const {id, value} = e.target;
+    const { id, value } = e.target;
     setForm((prevState) => ({
       ...prevState,
       [id]: value,
     }));
+  };
+  const handleCheckEmail = async (email:string) => {
+    try {
+      const response: AxiosResponse<verifyForm> = await API.post("/mail/reset-password", { email });
+      console.log("이메일 인증 코드 요청 : ",response.data);
+    } catch (e) {
+      console.log("error : ", e.response.data?.message);
+    }
   }
 
   return (
@@ -35,7 +46,7 @@ const ResetPassword: React.FC<LoginProps> = ({initialValues}) => {
               // placeholder="email 을 입력해주세요"
                    value={form.email}
             />
-            <button type="button">메일전송</button>
+            <button type="button" onClick={()=> handleCheckEmail(form.email)}>메일전송</button>
           </div>
         </div>
         <div className={styles.inputForm}>
