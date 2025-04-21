10efcc6ed2fdf092a390aa94a13dfacbb553397c
Jae-Hong-Kim <newfly101@naver.com>
2025-04-21 15:07:57 +0900

    chore: main 팀 없는 경우, 화면 렌더링 시 data 불러오게 변경
    ref: #18

---
+++ src/pages/main/MainNoTeam.interface.ts
@@ -0,0 +1,8 @@
+export interface teamDataResponse {
+  "teamId": any,
+  "teamName": String,
+  "level": String,
+  "startTime": String,
+  "problemCount": any,
+  "currentMembers": any,
+}
--- src/pages/main/MainNoTeam.tsx
+++ src/pages/main/MainNoTeam.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@/pages/common/Header.tsx';
 import Filter from '@/pages/common/Filter.tsx';
 import styles from "@/css/main/Layout.module.scss";
+import axios, { AxiosResponse } from 'axios';
+import API from '@/store/api/ApiConfig.ts';
+import { teamDataResponse } from '@/pages/main/MainNoTeam.interface.ts';
 
 const mainTeamData = [
-  {"name": "개 열심히 하죠", "time": "매일 오후 6시", "sksdleh" : "난이도 상 / 4문제", "people" : "인원 3/10"},
-  {"name": "천천히 하자", "time": "매일 오전 11시", "sksdleh" : "난이도 하 / 1문제", "people" : "인원 7/10"},
-  {"name": "개구락지", "time": "매일 오전 2시", "sksdleh" : "난이도 최상 / 3문제", "people" : "인원 5/10"},
-  {"name": "푸바오 못참지", "time": "매일 오후 8시", "sksdleh" : "난이도 중 / 2문제", "people" : "인원 2/10"},
-  {"name": "헤으응 개발", "time": "매일 오전 7시", "sksdleh" : "난이도 허 / 4문제", "people" : "인원 10/10"},
-  {"name": "초심을 찾자", "time": "매일 오전 3시", "sksdleh" : "난이도 하 / 6문제", "people" : "인원 9/10"},
-  {"name": "구름톤 스터디", "time": "매일 오후 11시", "sksdleh" : "난이도 중 / 7문제", "people" : "인원 0/10"},
-  {"name": "네이버 취업", "time": "매일 오후 3시", "sksdleh" : "난이도 상 / 9문제", "people" : "인원 1/10"},
-  {"name": "해커톤 모집중", "time": "매일 오후 1시", "sksdleh" : "난이도 하 / 19문제", "people" : "인원 4/10"},
+  {"teamId" : 1,"teamName": "개 열심히 하죠", "time": "매일 오후 6시", "level" : "상", "problemCount":"4", "currentMembers" : "3"},
+  {"teamId" : 2,"teamName": "천천히 하자", "time": "매일 오전 11시", "level" : "하", "problemCount":"1", "currentMembers" : "7"},
+  {"teamId" : 3,"teamName": "개구락지", "time": "매일 오전 2시", "level" : "최", "problemCount":"3", "currentMembers" : "5"},
+  {"teamId" : 4,"teamName": "푸바오 못참지", "time": "매일 오후 8시", "level" : "중", "problemCount":"2", "currentMembers" : "2"},
+  {"teamId" : 5,"teamName": "헤으응 개발", "time": "매일 오전 7시", "level" : "허", "problemCount":"4", "currentMembers" : "10"},
+  {"teamId" : 6,"teamName": "초심을 찾자", "time": "매일 오전 3시", "level" : "하", "problemCount":"6", "currentMembers" : "9"},
+  {"teamId" : 7,"teamName": "구름톤 스터디", "time": "매일 오후 11시", "level" : "중", "problemCount":"7", "currentMembers" : "0"},
+  {"teamId" : 8,"teamName": "구름톤 스터디", "time": "매일 오후 11시", "level" : "중", "problemCount":"7", "currentMembers" : "0"},
+  {"teamId" : 9,"teamName": "구름톤 스터디", "time": "매일 오후 11시", "level" : "중", "problemCount":"7", "currentMembers" : "0"},
 ]
 const MainNoTeam = () => {
+  const [isOpenDialog, setIsOpenDialog] = React.useState(false);
+
+  const handleOpenDialog = (index: number) => {
+    setIsOpenDialog(true);
+  }
+
+  useEffect(() => {
+    const fetchTeamData = async () => {
+      try {
+        console.log("화면 렌더링 중에 이게 완료되면 렌더링");
+        const response: AxiosResponse<teamDataResponse> = await API.get("/teams");
+        console.log("response",response);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchTeamData();
+  }, []);
+
   return (
     <div className={styles.mainContainer}>
       <Header />
@@ -23,12 +45,12 @@ const MainNoTeam = () => {
       </div>
       <section>
         {mainTeamData.map((item, index) => (
-        <div className={styles.teamComponent} key={`teams-${index}`}>
-            <button className={styles.teamContent}>
-              <h3>{item.name}</h3>
+        <div className={styles.teamComponent} key={`teams-${item.teamId}`}>
+            <button className={styles.teamContent} onClick={() => handleOpenDialog(index)}>
+              <h3>{item.teamName}</h3>
               <p>{item.time}</p>
-              <p>{item.sksdleh}</p>
-              <p>{item.people}</p>
+              <p>난이도 {item.level} / {item.problemCount}문제</p>
+              <p>인원 {item.currentMembers} / 10</p>
             </button>
         </div>
         ))}
