efe0125ad88340532614e593b9cab3fc481dcfd8
Jae-Hong-Kim <newfly101@naver.com>
2025-04-21 19:39:09 +0900

    style: team 생성 버튼 클릭 시 팀 생성 dialog 추가
    ref: #18

--- src/pages/main/component/TeamHeader.tsx
+++ src/pages/main/component/TeamHeader.tsx
@@ -3,16 +3,26 @@ import styles from '@/css/main/Layout.module.scss';
 import SearchBox from '@/pages/main/component/SearchBox.tsx';
 import FilterOrder from '@/pages/main/component/FilterOrder.tsx';
 import FilterQuestion from '@/pages/main/component/FilterQuestion.tsx';
+import TeamCreate from '@/pages/main/TeamDialog/TeamCreate.tsx';
 
 const TeamHeader = () => {
+  const [isOpenDialog, setIsOpenDialog] = React.useState(false);
+
+  const handleCreateTeam = () => {
+    setIsOpenDialog(true);
+  }
   return (
     <div className={styles.container}>
       <SearchBox />
       <section>
         <FilterOrder />
         <FilterQuestion />
-        <button className={styles.teamCreateButton}>팀 생성</button>
+        <button className={styles.teamCreateButton} onClick={() => handleCreateTeam()}>팀 생성</button>
       </section>
+
+      {isOpenDialog && (
+        <TeamCreate onClose={() => setIsOpenDialog(false)} />
+      )}
     </div>
   );
 };
