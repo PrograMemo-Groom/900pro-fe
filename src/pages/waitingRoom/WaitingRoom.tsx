d92faf095df1f5ec7a6bda129ccab9fcf46af10f
S2ej1n <ks320205@naver.com>
2025-04-24 04:10:10 +0900

    [Feat] 대기실 멤버 변수 추가

--- src/pages/waitingRoom/WaitingRoom.tsx
+++ src/pages/waitingRoom/WaitingRoom.tsx
@@ -1,7 +1,26 @@
+import styles from '@/css/waiting/waitingroom.module.scss'
+
 export default function WaitingRoom() {
+    const members = [
+        { name: "김재홍", status: "준비완료" },
+        { name: "강세진(나)", status: "대기중" },
+        { name: "이보미", status: "준비완료" },
+        { name: "김유림", status: "대기중" },
+        { name: "코딩고수", status: "준비완료" },
+        { name: "심동훈", status: "준비완료" },
+        { name: "김건영", status: "준비완료" },
+    ];
+
   return (
-    <div>
-      대기실입니다
-    </div>
+    <main className={styles.container}>
+        대기실
+        <h3>시작까지 남은 시간</h3>
+        <p className={styles.timer}>25:30</p>
+        <p className={styles.notice} >
+            *시험 시간이 되면 자동으로 화면이 이동되므로 5분 전까지 대기실에서 대기해주세요.</p>
+        <button className={styles.readyButton}>
+            준비하기
+        </button>
+    </main>
   )
 }
