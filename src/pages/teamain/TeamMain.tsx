705d5605df3aa3be0077b752f2ce88522f73b434
S2ej1n <ks320205@naver.com>
2025-04-25 06:25:34 +0900

    [Desing] 구조만 잡음

--- src/pages/teamain/TeamMain.tsx
+++ src/pages/teamain/TeamMain.tsx
@@ -4,25 +4,27 @@ export default function TeamMain() {
     <main className={styles.teamroom}>
         <section className={styles.left_container}>
             <section className={styles.container}>
-                <h2>프로그래모</h2>
-                <p>매일 오후 9시 | 중 | 3문제 | 2시간</p>
-                <div>
+                <div className={styles.team_info}>
+                    <h2>프로그래모</h2>
+                    <p>매일 오후 9시 | 중 | 3문제 | 2시간</p>
+                </div>
+                <div className={styles.team_des}>
                     <p>
                     안녕하세요 “프로그램(Program)”과 “Memo(기억, 기록)”를 합쳐, <br/>
-                    함께 개발하며 기억에 남는 성과를 만들어가는
-                    팀 <strong>“프로그래모(PrograMemo)”</strong> 입니다
+                    함께 개발하며 기억에 남는 성과를 만들어가는  팀 “프로그래모(PrograMemo)” 입니다
+
+                    <br/>--------------------------------------- <br/>
+                    매일 9시 출석체크 채널에 출석 :  <br/>책상 사진 찍어서 올리기 ( 지각 9:10까지 인정 ) <br/>
+                    🚨 올리지 않을 경우 패널티 :  <br/>랜덤 추첨 1명에게  <br/> ☕️커피사기 (메가커피,컴포즈 등등..)
+                    <br/>---------------------------------------- <br/>
+                    시험 참여 3번 이상시 강퇴합니다.<br/>
+                    멧돌멧돌멧돌이노래를한다아기손자며느리다모여서<br/>
+                    멧돌멧돌멧돌이노래를한다아기손자며느리다모여서<br/>
                     </p>
-                    <p>
-                    매일 9시 출석체크 채널에 출석<br/>
-                    책상 사진 찍어서 올리기 (지각 9:10까지 인정)
-                    <span role="img" aria-label="패널티">🚨</span> 올리지 않을 경우 패널티
-                        <br />- 랜덤 추첨 1명에게 ☕️커피사기 (메가커피, 컴포즈 등등..)
-                    </p>
-                    <p>시험 참여 3번 이상시 강퇴합니다.</p>
                 </div>
             </section>
             <footer>
-                <button>시험보러갈래잉</button>
+                <button className={styles.start_button}>시험에 입장할 수 없습니다.</button>
             </footer>
         </section>
         <aside className={styles.container}>
