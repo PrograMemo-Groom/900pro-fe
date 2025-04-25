fe460a2a1e6273f8f1ccb45b163c900613c951bc
S2ej1n <ks320205@naver.com>
2025-04-26 00:46:12 +0900

    [Feat] 재홍님 팀 없을경우 리스트 페이지 수정

--- src/pages/main/MainNoTeam.tsx
+++ src/pages/main/MainNoTeam.tsx
@@ -10,6 +10,7 @@ import { teamDataResponse } from '@/pages/main/MainNoTeam.interface.ts';
 import TeamHeader from '@/pages/main/component/TeamHeader.tsx';
 import TeamDetail from '@/pages/main/TeamDialog/TeamDetail.tsx';
 
+
 const mainTeamData = [
   {"teamId" : 1,"teamName": "개 열심히 하죠", "time": "매일 오후 6시", "level" : "상", "problemCount":"4", "currentMembers" : "3"},
   {"teamId" : 2,"teamName": "천천히 하자", "time": "매일 오전 11시", "level" : "하", "problemCount":"1", "currentMembers" : "7"},
@@ -50,27 +51,31 @@ const MainNoTeam = () => {
   }, []);
 
   return (
-    <div className={styles.mainContainer}>
+    <main>
       <Header />
-      <TeamHeader />
-      <section>
-        {teamList.length === 0 ? (
-          <p> 가입 가능한 팀이 없습니다.</p>
-        ) : (
-          teamList.map((item, index) => (
-        <article className={styles.teamCard} key={`teams-${item.teamId}`} onClick={() => handleOpenDialog(index)}>
-              <header>{item.teamName}</header>
-              <p>{item.time}</p>
-              <p>난이도 {item.level} / {item.problemCount}문제</p>
-              <p>인원 {item.currentMembers} / 10</p>
-        </article>
-        )))}
-      </section>
 
-      {isOpenDialog && (
-        <TeamDetail team={teamList[selectedIndex]} onClose={() => setIsOpenDialog(false)} />
-      )}
-    </div>
+      <div className={styles.mainContainer}>
+        {/* <Header /> */}
+        <TeamHeader />
+        <section>
+          {teamList.length === 0 ? (
+            <p> 가입 가능한 팀이 없습니다.</p>
+          ) : (
+            teamList.map((item, index) => (
+          <article className={styles.teamCard} key={`teams-${item.teamId}`} onClick={() => handleOpenDialog(index)}>
+                <header>{item.teamName}</header>
+                <p>{item.time}</p>
+                <p>난이도 {item.level} / {item.problemCount}문제</p>
+                <p>인원 {item.currentMembers} / 10</p>
+          </article>
+          )))}
+        </section>
+
+        {isOpenDialog && (
+          <TeamDetail team={teamList[selectedIndex]} onClose={() => setIsOpenDialog(false)} />
+        )}
+      </div>
+    </main>
   );
 };
 
