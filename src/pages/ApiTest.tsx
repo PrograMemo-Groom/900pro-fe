eb155a6fed6172013091b5d9f872b2af0773fc15
saaad99 <ehdgnstla@naver.com>
2025-04-27 21:32:32 +0900

    github actions 테스트

--- src/pages/ApiTest.tsx
+++ src/pages/ApiTest.tsx
@@ -19,7 +19,7 @@ const Login = () => {
         <h2> 연결 테스트</h2>
         <div>
             <img src="./vite.svg" alt="logo"/>
-            <div>받아온 값 : {data}</div>
+            <div>받아온 값 : ?  {data}</div>
         </div>
     </div>);
 };
